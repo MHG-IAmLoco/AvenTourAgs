40fdd62f9571e349a0c2765884945e820f2d1255
Manuel Hernández Gutiérrez <iamloco@outlook.com>
2018-05-07 22:55:26 -0500

    map 1/2

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,6 +1,6 @@
 import { Geolocation } from '@ionic-native/geolocation';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, ActionSheetController } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-mapa',
@@ -13,7 +13,8 @@ export class MapaPage {
   public zoom: number = 16;
   public dir = undefined;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _geoLoc: Geolocation) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _geoLoc: Geolocation,
+    public platform: Platform, public actionsheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -41,6 +42,97 @@ export class MapaPage {
     // });
   }
 
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Destino',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Aguascalientes',
+          handler: () => {
+            let actionSheet = this.actionsheetCtrl.create({
+              title: 'Aguascalientes',
+              buttons: [
+                {
+                  text: 'Jardín de San Marcos',
+                  handler: () => {
+                    this.dir = {
+                      origin: { lat: this.lat, lng: this.lng },
+                      destination: { lat: 21.8794981, lng: -102.3030917 }
+                    }
+                  }
+                },
+                {
+                  text: 'Plaza de Armas',
+                  handler: () => {
+                    this.dir = {
+                      origin: { lat: this.lat, lng: this.lng },
+                      destination: { lat: 21.8806672, lng: -102.2961909 }
+                    }
+                  }
+                },
+                {
+                  text: 'Cancelar',
+                  role: 'cancel', // will always sort to be on the bottom
+                  icon: !this.platform.is('ios') ? 'close' : null,
+                  handler: () => {
+                    console.log('Cancel clicked');
+                  }
+                }
+              ]
+            });
+            actionSheet.present();
+          }
+        },
+        {
+          text: 'Calvillo',
+          handler: () => {
+            let actionSheet = this.actionsheetCtrl.create({
+              title: 'Calvillo',
+              buttons: [
+                {
+                  text: 'Plaza',
+                  handler: () => {
+                    this.dir = {
+                      origin: { lat: this.lat, lng: this.lng },
+                      destination: { lat: 21.8463772, lng: -102.7186531 }
+                    }
+                  }
+                },
+                {
+                  text: 'Presa de Malpaso',
+                  handler: () => {
+                    this.dir = {
+                      origin: { lat: this.lat, lng: this.lng },
+                      destination: { lat: 21.8577234, lng: -102.6534021 }
+                    }
+                  }
+                },
+                {
+                  text: 'Cancelar',
+                  role: 'cancel', // will always sort to be on the bottom
+                  icon: !this.platform.is('ios') ? 'close' : null,
+                  handler: () => {
+                    console.log('Cancel clicked');
+                  }
+                }
+              ]
+            });
+            actionSheet.present();
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 
 }
 

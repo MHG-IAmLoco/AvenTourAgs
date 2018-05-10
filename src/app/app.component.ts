cd56e5e231a6c59008b3948d6b654076b78ad98b
Manuel Hernández Gutiérrez <iamloco@outlook.com>
2018-05-10 16:41:52 -0500

    Qr test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import {LoginPage} from '../pages/login/login';
+import {QrPage} from '../pages/qr/qr';
 @Component({
   templateUrl: 'app.html'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,12 @@ import { RegistroPageModule } from '../pages/registro/registro.module';
 import { GaleriaPageModule } from '../pages/galeria/galeria.module';
 import { MenuGalPageModule } from '../pages/menu-gal/menu-gal.module';
 import { MapaPageModule } from '../pages/mapa/mapa.module';
+import { QrPageModule } from '../pages/qr/qr.module'
 import { ListaTicketsPageModule } from '../pages/lista-tickets/lista-tickets.module';
 import { InfoEventoPageModule } from '../pages/info-evento/info-evento.module';
 import { SeleccionPageModule } from '../pages/seleccion/seleccion.module';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -37,10 +40,12 @@ import { HomePage } from '../pages/home/home';
     GaleriaPageModule,
     BrowserModule,
     MapaPageModule,
+    QrPageModule,
     HttpClientModule,
     MenuTicketsPageModule,
     ListaTicketsPageModule,
     RegistroPageModule,
+    NgxQRCodeModule,
     IonicModule.forRoot(MyApp),
     InfoEventoPageModule,
     SeleccionPageModule
@@ -57,6 +62,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     Geolocation,
+    BarcodeScanner,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })

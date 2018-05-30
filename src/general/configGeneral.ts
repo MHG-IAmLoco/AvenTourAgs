9b869cca35cda84f8fd076ebdf7b462b3a9e9320
Manuel Hernández Gutiérrez <iamloco@outlook.com>
2018-05-29 22:05:55 -0500

    Commit

--- src/general/configGeneral.ts
+++ src/general/configGeneral.ts
@@ -10,8 +10,10 @@ export class ConfigGeneral{
     //public strUrlImages:string="http://192.168.137.1:8082/media/";
 
     //Locales
-    public strUrlApis:string="http://192.168.1.66:5000/api/general/";
-    public strUrlImages:string="http://192.168.1.66:80/media/";
+   // public strUrlApis:string="http://192.168.1.66:5000/api/general/";
+    //public strUrlImages:string="http://192.168.1.66:80/media/";
+    public strUrlApis:string="http://172.16.109.4:5000/api/general/";
+    public strUrlImages:string="http://172.16.109.4:80/media/";
    // public strUrlApis:string="http://localhost:5000/api/general/";
     //public strUrlImages:string="http://localhost:80/media/";
 
